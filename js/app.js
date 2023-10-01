26f96f29be1343422fb87fde16e5a18e8e806854
shavvvv <shavaungreen102@gmail.com>
2023-10-01 16:00:48 -0400

    Question 2

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ Set the content of mainEl to <h1>SEI Rocks!</h1>.  */
 
 let newH1= document.createElement('h1')
 
-console.log(newH1)
+// console.log(newH1)
 
 let text= 'SEI Rocks!'
 
@@ -50,5 +50,33 @@ mainEl.classList.add('flex-ctr') ;
 /**Task 2.0
 Select and cache the <nav id="top-menu"> element in a variable 
 named topMenuEl. */
+
+
+const topMenuEl = document.getElementById('top-menu')
+
+
+
+
+/**Task 2.1
+Set the height topMenuEl element to be 100%. */
+
+topMenuEl.style.height='100%'
+
+
+
+
+
+/**Task 2.2
+Set the background color of topMenuEl using the
+ --top-menu-bg CSS custom property. */
+
+
+topMenuEl.style.backgroundColor= 'var(--top-menu-bg)'
+
+
+/** Task 2.3
+Add a class of flex-around to topMenuEl. */
+
+topMenuEl.classList.add('flex-around') 
 
 
